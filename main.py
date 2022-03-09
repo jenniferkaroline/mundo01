19df85469b142e54da03b3aedecf9a4b0f0c8172
JenniferKarolin <>
2022-03-09 00:37:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import mundo01
+
+mundo01.desafio001()
+
+
+mundo01.desafio002()
+
+
+
+mundo01.desafio003()
+
+
+
+
+
+mundo01.desafio004()
+
+
+
+
+
+
+
+
+
+
+mundo01.desafio005()
---
+++ mundo01.py
@@ -0,0 +1,47 @@
+
+
+def desafio001():
+  print('olá mundo!')
+
+def desafio002():
+  nome = input('escreva seu nome:')
+  print('Seja bem vindo(a)',nome)
+
+def desafio003():
+  x1 = int(input("digite um número: "))
+  x2 = int(input("digite outro número: "))
+  s = x1+x2
+  print('a soma vale: ',s)
+
+def desafio004():
+  a = input("digite algo: ")
+  print("o tipo primitivo desse valor é ", type(a))
+  print("só tem espaços? ", a.isspace())
+  print("é um número? ", a.isnumeric())
+  print("é alfabético? ", a.isalpha())
+  print("é alfanumérico? ", a.isalnum())
+  print("está em maíusculas? ", a.isupper())
+  print("está em minúsculas? ", a.islower())
+  print("está capitalizada? ", a.istitle())
+
+def desafio005():
+  
+  
+
+
+  
+
+
+
+
+
+  
+  
+
+
+
+  
+
+
+
+
