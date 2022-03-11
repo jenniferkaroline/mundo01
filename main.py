15964cc42e2049483b71476976a6d23a02ee40ca
Jennifer <99359081+jenniferkaroline@users.noreply.github.com>
2022-03-11 01:36:12 +0000

    mais desafios

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import mundo01
 
 mundo01.desafio001()
 
-
 mundo01.desafio002()
-
-
 
 mundo01.desafio003()
 
-
-
-
-
 mundo01.desafio004()
 
-
-
-
-
-
-
-
-
-
 mundo01.desafio005()
+
+mundo01.desafio006()
+
+mundo01.desafio007()
+
+mundo01.desafio008()
+
+mundo01.desafio009()
+
+
+
+
+
+
+
+
--- mundo01.py
+++ mundo01.py
@@ -10,7 +10,7 @@ def desafio002():
 def desafio003():
   x1 = int(input("digite um número: "))
   x2 = int(input("digite outro número: "))
-  s = x1+x2
+  s = x1 + x2
   print('a soma vale: ',s)
 
 def desafio004():
@@ -25,6 +25,37 @@ def desafio004():
   print("está capitalizada? ", a.istitle())
 
 def desafio005():
+  n1 = int(input("me dê um número: "))
+  an1 = n1 - 1
+  sn1 = n1 + 1
+  print('analisando o valor {}, seu antecessor é {}, e seu sucessor é {}'.format(n1,an1,sn1))
+
+def desafio006():
+  n2 = int(input("me dê um número: "))
+  dn2 = n2 * 2
+  tn2 = n2 * 3
+  rqn2 = n2 ** (1/2)
+  print('analisando o valor {}, seu dobro é {}, seu triplo é {}, e sua raíz quadrada é {}'.format(n2,dn2,tn2,rqn2))
+
+def desafio007():
+  nota1 = float(input("digite sua primeira nota: "))
+  nota2 = float(input("digite sua segunda nota: "))
+  média = (nota1 + nota2)/2
+  print('a média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(nota1,nota2,média))
+
+def desafio008():
+  medida = float(input("me dê uma distância em metros: "))
+  cm = medida * 100
+  mm = medida * 1000
+  print('a medida de {}m é igual a {:.0f}cm e {:.0f}mm'.format(medida,cm,mm))
+
+def desafio009():
+  
+  
+  
+  
+  
+  
   
   
 
